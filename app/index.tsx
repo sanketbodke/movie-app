3bc59dc87644a7be835ab8b6527f8934c81db7b7
Sanket Bodke <84568517+sanketbodke@users.noreply.github.com>
2025-04-26 22:13:07 +0530

    nativewind setup

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className={"flex justify-center items-center h-full"}>
+      <Text className={"text-accent text-5xl"}>Welcome!</Text>
+    </View>
+  );
+}
