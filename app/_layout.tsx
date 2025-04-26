d822d232a31c860b431ae5a9f7171aa3c828b040
Sanket Bodke <84568517+sanketbodke@users.noreply.github.com>
2025-04-26 23:58:36 +0530

    add: assets add: tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,22 @@ import { Stack } from "expo-router";
 import "./globals.css"
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+      <Stack>
+        {/*Hide route groupe header*/}
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen
+            name="movies/[id]"
+            options={{
+              headerShown: false,
+            }}
+        />
+      </Stack>
+  );
 }
