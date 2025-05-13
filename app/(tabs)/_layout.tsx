1fd09dfaa1a85281d76a7f606474815cc8440e31
Sanket Bodke <84568517+sanketbodke@users.noreply.github.com>
2025-05-13 22:06:10 +0530

    fix TabIcon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ const TabIcon = ({focused, icon, title}: any) => {
             <ImageBackground
                 // @ts-ignore
                 source={images.highlight}
-                className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+                className="flex flex-row w-full flex-1 min-w-[200px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
             >
                 <Image source={icon} tintColor="#151312" className="size-5" />
                 <Text className="text-secondary text-base font-semibold ml-2">
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css"
 export default function RootLayout() {
   return (
       <Stack>
-        {/*Hide route groupe header*/}
+        {/*Hide route group header*/}
         <Stack.Screen
           name="(tabs)"
           options={{
